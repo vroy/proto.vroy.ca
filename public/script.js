73fa338f502114745bee308fddd063e6696e7dc8
Vincent Roy <vincentroy8@gmail.com>
2011-05-18 13:54:18 -0300

    Add a footer to proto.vroy.ca that is based vroy.ca.

--- public/script.js
+++ public/script.js
@@ -1,3 +1,10 @@
+(function() {
+    // Get the footer from vroy.ca. The footer.js script will inject the footer into #footer if it exists.
+    var ext = document.createElement('script'); ext.type = 'text/javascript'; ext.async = true;
+    ext.src = 'http://vroy.ca/footer.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ext);
+})();
+
 $(document).ready(function(){
   
   
